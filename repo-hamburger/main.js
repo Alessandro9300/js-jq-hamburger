bf7d472168716e3f1d76c3b705a548747f0bba80
Alessandro9300 <aandreoli93@gmail.com>
2020-03-31 19:11:51 +0200

    fatto ex

---
+++ repo-hamburger/main.js
@@ -0,0 +1,38 @@
+// Creare un hamburger menu con l’utilizzo di Html, Css e JavaScript/jQuery
+// prendo lo zip che dopo vi allego e senza toccare l’HTML faccio funzionare l’hamburger menù;
+// primo click, appare il menù;
+// se cliccko che è già visibile, invece scompare.
+
+// Seleziono le classi da modificare
+
+var burgerA = $(".hamburger-menu a");
+var burgerMenu = $(".hamburger-menu");
+var classX = $(".fa-times");
+var burgerMini = $(".fa-bars")
+
+// Creo una variabile per far funzionare la condizione
+
+var trueBurger = false;
+
+// Apporto le modifiche necessarie affinchè la classe si possa visualizzare correttamente
+
+burgerMenu.css({"min-height": "unset", "height": "unset", "line-height": 2, "min-width": "unset", "width": 250, "list-style-type":"none", "background-color": "white", "box-shadow": "1px 1px 1px #E78D5C33", "border": "solid #E78D5C33 0.5px", "position": "absolute", "top": 35, "right": 40, "left": "unset", "border-radius": 7});
+
+burgerA.css({"font-size": 20, "text-decoration": "none", "border-bottom": "none", "color": "#565A5C"})
+
+classX.css({"display": "none"})
+
+
+// Creo una condizione per far apparire e scomparire il menu burger
+
+burgerMini.click(function(){
+
+  if (trueBurger === false){
+    burgerMenu.show();
+    trueBurger = true;
+    console.log(trueBurger);
+  } else if (trueBurger){
+    burgerMenu.hide();
+    trueBurger = false;
+  }
+})
