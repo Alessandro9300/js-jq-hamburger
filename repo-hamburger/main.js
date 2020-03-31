08b39d65914652ee0b7c319fc512ef92a3dd376a
Alessandro9300 <aandreoli93@gmail.com>
2020-03-31 19:29:03 +0200

    sistemato ex

--- repo-hamburger/main.js
+++ repo-hamburger/main.js
@@ -16,11 +16,12 @@ var trueBurger = false;
 
 // Apporto le modifiche necessarie affinchè la classe si possa visualizzare correttamente
 
+// burgerMenu.show();
+
 burgerMenu.css({"min-height": "unset", "height": "unset", "line-height": 2, "min-width": "unset", "width": 250, "list-style-type":"none", "background-color": "white", "box-shadow": "1px 1px 1px #E78D5C33", "border": "solid #E78D5C33 0.5px", "position": "absolute", "top": 35, "right": 40, "left": "unset", "border-radius": 7});
 
 burgerA.css({"font-size": 20, "text-decoration": "none", "border-bottom": "none", "color": "#565A5C"})
 
-classX.css({"display": "none"})
 
 
 // Creo una condizione per far apparire e scomparire il menu burger
@@ -32,6 +33,15 @@ burgerMini.click(function(){
     trueBurger = true;
     console.log(trueBurger);
   } else if (trueBurger){
+    burgerMenu.hide();
+    trueBurger = false;
+  }
+})
+
+// creo funzione per chiudere il menu a tendina anche quando è sopra il 1000px
+
+classX.on({
+  click: function(){
     burgerMenu.hide();
     trueBurger = false;
   }
